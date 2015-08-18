5f7346027900e6fee73a5243f30859bac58ad9b3
bbenny0211 <benulabenny@gmail.com>
2015-08-18 14:43:17 +0100

    Running robot/chess.py

--- robot/chess.py
+++ robot/chess.py
@@ -6,7 +6,6 @@ from tealight.robot import (move,
                             left_side, 
                             right_side)
 
-def robot (side):
-    for i in range(0,3):
-    move(side)
-    turn(120)
+move
+turn(1)
+    
