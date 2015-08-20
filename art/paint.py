b804e464a878f8e0cdb860916b37b061ed99bf13
bbenny0211 <benulabenny@gmail.com>
2015-08-20 12:01:09 +0100

    Running art/paint.py

--- art/paint.py
+++ art/paint.py
@@ -2,27 +2,29 @@ from tealight.art import (color,rectangle, line, spot, circle, box, image, text,
 
 color("black")
 
-rectangle(10,250,25,25)
-spot(22,260,3)
-
-
-rectangle(10,275,25,25)
-spot(22,290,4)
-
 rectangle(10,300,25,25)
-spot(22,315,5)
+spot(22,310,3)
 
 rectangle(10,325,25,25)
-spot(22,340,6)
+spot(22,340,4)
 
 rectangle(10,350,25,25)
-spot(22,365,7)
+spot(22,365,5)
 
 rectangle(10,375,25,25)
-spot(22,390,8)
+spot(22,390,6)
+
+rectangle(10,400,25,25)
+spot(22,415,7)
+
+rectangle(10,425,25,25)
+spot(22,440,8)
 
 def handle_mousedown(x,y):
   ColNo = (x - 10)/25
   RowNo = (y - 75)/25
   
   print ColNo, RowNo
+  
+  if ColNo == 0 and RowNo == 7:
+    print 
