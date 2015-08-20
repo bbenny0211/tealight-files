23f99219acff90b07da31237d86cefa33d6dfe11
bbenny0211 <benulabenny@gmail.com>
2015-08-20 14:04:47 +0100

    Saving art/alexinder.py

--- art/alexinder.py
+++ art/alexinder.py
@@ -1,1 +1,26 @@
-import github.alexinder.art.alexpaint
+from tealight.art import (color, line, spot, circle, box, image, text, background)
+
+def DrawPalette(x,y, colors, w, h):
+  for c in colors:
+    color(c)
+    box(x, y, w, h)
+    y = y + h
+ 
+ colors = ["black",
+           "grey".
+           ]
+ 
+x = 100
+y = 75
+w = 25
+DrawPalette(x,y, colors, w, h)
+
+def handle_mousedown(mx,my):
+  if mx>x and mx < x+w and my>y and my < y+(h*len(colors)):
+    
+    RowNo = (my-y)/h
+    
+    print RowNo
+    print colors[RowNo]
+   
+   return
