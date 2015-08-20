7978a97f3f18106722defb6f67f271330d164595
bbenny0211 <benulabenny@gmail.com>
2015-08-20 15:11:23 +0100

    Running art/paint.py

--- art/paint.py
+++ art/paint.py
@@ -2,7 +2,7 @@ from tealight.art import (color,rectangle, line, line_width, spot, circle, box, 
 
 color("black")
 
-sizes = range (3,33,3)
+sizes = range (3,36,3)
 
 def ToolBoxThickness (x,y,thicknesses,w,h):
   for t in thicknesses:
