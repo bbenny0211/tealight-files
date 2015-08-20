ae3407647d5a783d7ef1a04a5f0634c7cb7e6847
bbenny0211 <benulabenny@gmail.com>
2015-08-20 10:57:37 +0100

    Running art/paint.py

--- art/paint.py
+++ art/paint.py
@@ -2,11 +2,6 @@ from tealight.art import (color,rectangle, line, spot, circle, box, image, text,
 
 color("black")
 
-spot(300,200,30)
-circle(300,200,50)
-box(250,300,100,100)
-
-
 rectangle(10,250,25,25)
 spot(22,260,3)
 
