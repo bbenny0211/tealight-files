c1c03c1d0b194f8b011ff1226485c6839243de6d
bbenny0211 <benulabenny@gmail.com>
2015-08-20 14:20:19 +0100

    Running art/paint.py

--- art/paint.py
+++ art/paint.py
@@ -2,24 +2,6 @@ from tealight.art import (color,rectangle, line, spot, circle, box, image, text,
 
 color("black")
 
-rectangle(10,300,25,25)
-spot(22,310,3)
-
-rectangle(10,325,25,25)
-spot(22,340,4)
-
-rectangle(10,350,25,25)
-spot(22,365,5)
-
-rectangle(10,375,25,25)
-spot(22,390,6)
-
-rectangle(10,400,25,25)
-spot(22,415,7)
-
-rectangle(10,425,25,25)
-spot(22,440,8)
-
 sizes = range (3,9)
 
 def ToolBoxThickness (x,y,thicknesses,w,h):
@@ -34,20 +16,6 @@ w = 25
 h = 25
 ToolBoxThickness(x,y,sizes,w,h)
     
-  
-
-
-
-
-
-
-
-
-
-
-
-  
-
 def handle_mousedown(x,y):
   ColNo = (x - 10)/25
   RowNo = (y - 300)/25
